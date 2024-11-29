4e13d4bc7e64f506dc9391fdeeb3fb6cb8142ecb
Ankit shetty <ankittshetty@gmail.com>
2024-11-29 14:10:15 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -25,6 +25,8 @@ app.use(
     credentials: true,
   })
 );
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
